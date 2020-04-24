869da64e4507d0aa2a25bd9e870e4b0c2fe4ecd8
jdmunera <davidmuner@hotmail.com>
2020-04-23 22:43:51 -0500

    redis

---
+++ src/main/java/integration/sf/mag/IntegrationSfBolsasApplication.java
@@ -0,0 +1,13 @@
+package integration.sf.mag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationSfBolsasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationSfBolsasApplication.class, args);
+	}
+
+}
